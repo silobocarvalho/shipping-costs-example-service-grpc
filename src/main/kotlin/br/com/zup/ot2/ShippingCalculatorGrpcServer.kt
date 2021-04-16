66bfdbef57e792ac81f71fd672a466214036dc2c
Sidartha <silobocarvalho@gmail.com>
2021-04-16 15:38:29 -0300

    Error msgs added

--- src/main/kotlin/br/com/zup/ot2/ShippingCalculatorGrpcServer.kt
+++ src/main/kotlin/br/com/zup/ot2/ShippingCalculatorGrpcServer.kt
@@ -1,7 +1,10 @@
 package br.com.zup.ot2
 
 import io.grpc.ServerBuilder
+import io.grpc.Status
 import io.grpc.stub.StreamObserver
+import java.lang.IllegalStateException
+import kotlin.random.Random
 
 
 fun main(){
@@ -23,6 +26,46 @@ class ShippingCalculatorGrpcServer : ShippingCostsServiceGrpc.ShippingCostsServi
         request: CalculateShippingCostsRequest?,
         responseObserver: StreamObserver<CalculateShippingCostsResponse>?
     ) {
+    //assume that request is not null with !! operator
+        val zipcode = request!!.zipcode
+
+        //Zipcode validation block
+        zipcode.let {
+            if(it == null || it.isBlank()){
+                val error = Status.INVALID_ARGUMENT
+                    .withDescription("Zipcode must be filled")
+                    .asRuntimeException()
+                responseObserver?.onError(error)
+            }
+
+            if(!it.matches("[0-9]{5}-[0-9]{3}".toRegex())){
+                responseObserver?.onError((
+                        Status.INVALID_ARGUMENT
+                            .withDescription("Invalid zipcode")
+                            .augmentDescription("Should follow: 11111-000 format")
+                            .asRuntimeException()
+                        ))
+                return
+            }
+        }
+
+        //Some errors should not be returned to client, for that, we use internal error handler.
+
+        var value = 0.0
+        try {
+            value = Random.nextDouble(from = 0.0, until = 140.0)
+            if(value > 100){
+                throw IllegalStateException("Simulated error with confidential data!")
+            }
+        }catch(e: Exception){
+            println(e)
+            responseObserver?.onError(Status.INTERNAL
+                .withDescription(e.message) // sent to client
+                .withCause(e) //not sent to client : type of exception: java.lang.IllegalStateException
+                .asRuntimeException() )
+        }
+
+
         val response = CalculateShippingCostsResponse.newBuilder()
             .setZipcode(request?.zipcode)
             .setPrice(150.22)
